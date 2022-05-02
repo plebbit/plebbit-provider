d2ea1a8f780d01d202895beace161151a7741a62
Esteban Abaroa <estebanabaroa@protonmail.com>
2022-05-02 22:12:11 +0000

    improve test

--- test-local.js
+++ test-local.js
@@ -1,15 +1,24 @@
 const IpfsHttpClient = require('ipfs-http-client')
 const http = require('http')
+const {toString} = require('uint8arrays/to-string')
 
 const agent = new http.Agent({keepAlive: true, maxSockets: 99999})
 const url = 'http://localhost:8080/api/v0'
 const client = IpfsHttpClient.create({agent, url})
 
 ;(async () => {
-  const res = await client.pubsub.subscribe('plebbit test')
-  console.log(res)
+  const onMessageReceived = (message) => {
+    console.log(`received message from ${message.from}:`, toString(message.data))
+  }
+  await client.pubsub.subscribe('plebbit test', onMessageReceived)
   setInterval(async () => {
-    const res = await client.pubsub.publish('plebbit test', Buffer.from(new Date().toISOString()))
-    console.log(res)
+    const message = new Date().toISOString()
+    try {
+      await client.pubsub.publish('plebbit test', Buffer.from(message))
+      console.log('published message:', message)
+    }
+    catch (e) {
+      console.log(e)
+    }
   }, 1000)
 })()
--- test-remote.js
+++ test-remote.js
@@ -1,6 +1,7 @@
 const IpfsHttpClient = require('ipfs-http-client')
 const https = require('https')
 const ProxyAgent = require('https-proxy-agent')
+const {toString} = require('uint8arrays/to-string')
 
 // const agent = new ProxyAgent('http://user:pass@111.111.111.111:8080')
 const agent = new https.Agent({keepAlive: true, maxSockets: 99999})
@@ -8,10 +9,18 @@ const url = 'https://pubsubprovider.xyz/api/v0'
 const client = IpfsHttpClient.create({agent, url})
 
 ;(async () => {
-  const res = await client.pubsub.subscribe('plebbit test')
-  console.log(res)
+  const onMessageReceived = (message) => {
+    console.log(`received message from ${message.from}:`, toString(message.data))
+  }
+  await client.pubsub.subscribe('plebbit test', onMessageReceived)
   setInterval(async () => {
-    const res = await client.pubsub.publish('plebbit test', Buffer.from(new Date().toISOString()))
-    console.log(res)
+    const message = new Date().toISOString()
+    try {
+      await client.pubsub.publish('plebbit test', Buffer.from(message))
+      console.log('published message:', message)
+    }
+    catch (e) {
+      console.log(e)
+    }
   }, 1000)
 })()
