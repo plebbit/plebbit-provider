66633c4b4c35d1796f9c54ea99c0cfe3b10012b5
Esteban Abaroa <estebanabaroa@protonmail.com>
2022-07-20 01:51:15 +0000

    auto reboot server on fail

--- scripts/monitor-on-fail.js
+++ scripts/monitor-on-fail.js
@@ -2,11 +2,21 @@
 const telegramBot = require('../telegram-bot')
 const {Telegram} = require('telegraf')
 const telegram = new Telegram(telegramBot.botToken)
+const rebootServer = require('./reboot-server')
 
-const message = () => 'pubsub provider is down ' + new Date().toISOString()
+const isDownMessage = () => 'pubsub provider is down ' + new Date().toISOString()
+const rebootedMessage = () => 'pubsub provider server rebooted ' + new Date().toISOString()
 
 const onFail = async () => {
-  await telegram.sendMessage(telegramBot.chatId, message())
+  try {
+    await telegram.sendMessage(telegramBot.chatId, isDownMessage())
+  }
+  catch (e) {
+    console.log(e)
+  }
+
+  await rebootServer()
+  await telegram.sendMessage(telegramBot.chatId, rebootedMessage())
 }
 
 module.exports = onFail
