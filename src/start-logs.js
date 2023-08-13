20027302f7c94676825276867c7fb913e83f4fa7
Esteban Abaroa <estebanabaroa@protonmail.com>
2023-08-13 20:11:34 +0000

    debug pubsub logs

--- src/start-logs.js
+++ src/start-logs.js
@@ -83,11 +83,11 @@ const writeLog = async (subplebbitAddress, log) => {
     const sorted = {}
     sorted.type = message.type
     sorted.challengeRequestId = message.challengeRequestId
+      console.log(message.challengeRequestId)
     try {
       sorted.challengeRequestId = toString(message.challengeRequestId, 'base58btc')
     }
     catch (e) {
-      console.log(message.challengeRequestId)
       console.log(e)
     }
     log = JSON.stringify({...sorted, ...message})
